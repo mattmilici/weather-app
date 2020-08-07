3fa247557689ffbee2d0f943829fc24993396fa7
Matthew Milici <milici.m915@gmail.com>
2020-08-07 08:24:00 -0700

    added UV Index

--- script.js
+++ script.js
@@ -71,7 +71,11 @@ $(document).ready(function() {
                         "&units=imperial&APPID=e6f4fe18625540e700ef27c1af0d7303",
                     method: "GET",
                 }).then(function(response) {
+                    console.log(response);
                     for (let f = 1; f < 6; f++) {
+                        let todayUVI = response.daily[0].uvi;
+                        $("#currentCityUV").text(todayUVI);
+
                         let forecastDateId = ".forecastDate" + f;
                         let forecastIconId = ".forecastIcon" + f;
                         let forecastTempId = ".forecastTemp" + f;
