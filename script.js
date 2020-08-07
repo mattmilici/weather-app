c9339b4add806c3c48fefafd923ed57eeb36d0e6
Matthew Milici <milici.m915@gmail.com>
2020-08-07 08:13:21 -0700

    added forecast dates

--- script.js
+++ script.js
@@ -65,7 +65,27 @@ $(document).ready(function() {
                         "&units=imperial&APPID=e6f4fe18625540e700ef27c1af0d7303",
                     method: "GET",
                 }).then(function(response) {
-                    console.log(response);
+                    for (let f = 1; f < 6; f++) {
+                        let forecastDateId = ".forecastDate" + f;
+                        let forecastIconId = ".forecastIcon" + f;
+                        let forecastTempId = ".forecastTemp" + f;
+                        let forecastHumidityId = ".forecastHumidity" + f;
+                        // ---------------------------forecasted date ---------------------------
+                        let forecastDate = response.daily[f].dt;
+                        var timeStap = moment.unix(forecastDate).format("MM/DD/YYYY");
+                        $(forecastDateId).text(timeStap);
+                        // ---------------------------forecasted icon ---------------------------
+                        let forecastIcon = response.daily[f].weather[0].icon;
+                        let forecastIconURL =
+                            "http://openweathermap.org/img/wn/" + forecastIcon + ".png ";
+                        $(forecastIconId).attr("src", forecastIconURL);
+                        // ---------------------------forecasted temp ---------------------------
+                        let forecastTemp = response.daily[f].temp.day;
+                        $(forecastTempId).text(forecastTemp);
+                        // ---------------------------forecasted humidity ---------------------------
+                        let forecastHumidity = response.daily[f].humidity;
+                        $(forecastHumidityId).text(forecastHumidity);
+                    }
                 });
             });
         }
@@ -85,3 +105,12 @@ $(document).ready(function() {
         callCityData();
     }
 });
+
+{
+    /* <div class="forecastCard col-lg-2 ">
+<p class="forecastDate0"></p>
+<p class="ForecastIcon0"></p>
+<p class="temp">Temp <span id="forecastTemp0"></span> &#8457</p>
+<p class="temp">Humiditiy <span class="forecastHumidity0"></span></p>
+</div> */
+}
