8419237a9070a3c3625b2b435eeb00528dbb6a56
Matthew Milici <milici.m915@gmail.com>
2020-08-06 18:39:24 -0700

    added weekly forecast API

--- script.js
+++ script.js
@@ -1,78 +1,87 @@
 $(document).ready(function() {
-
     // --------------------------- Global Variables ---------------------------
     var userTopCitiesArray = JSON.parse(localStorage.getItem("userCities")) || [];
-    var userSearch = $("#cityInput ").val()
+    var userSearch = $("#cityInput ").val();
+    var cityLat;
+    var cityLon;
 
     // --------------------------- Event Listeners ---------------------------
-    $("#submitButton").on("click", callCityData)
+    $("#submitButton").on("click", callCityData);
 
     // --------------------------- Gathers first five cities on the users Array List ---------------------------
-    favriteCities()
+    favriteCities();
 
     // --------------------------- Populates main screen with users most recent searched city ---------------------------
-    callCityData()
+    callCityData();
 
     // --------------------------- updates the users main city view ---------------------------
     function callCityData() {
         if (userSearch !== "") {
-            userSearch = $("#cityInput").val()
+            userSearch = $("#cityInput").val();
         } else if (userTopCitiesArray > 0) {
-            userSearch = userTopCitiesArray[0]
-        } else { userSearch = "greensburg" }
+            userSearch = userTopCitiesArray[0];
+        } else {
+            userSearch = "greensburg";
+        }
 
-        values = userSearch.split(', ');
+        values = userSearch.split(", ");
         let userCity = values[0];
         let userState = values[1];
 
         if (userSearch !== "") {
             $.ajax({
-                url: 'http://api.openweathermap.org/data/2.5/weather?q=' + userCity + '&units=imperial&APPID=e6f4fe18625540e700ef27c1af0d7303',
+                url: "http://api.openweathermap.org/data/2.5/weather?q=" +
+                    userCity +
+                    "&units=imperial&APPID=e6f4fe18625540e700ef27c1af0d7303",
                 method: "GET",
-
             }).then(function(response) {
-
                 let cityName = response.name;
                 let cityTemp = response.main.temp;
                 let humidity = response.main.humidity;
                 let windSpeed = response.wind.speed;
-
-                console.log(cityName)
+                console.log(response);
 
                 let iconCode = response.weather[0].icon;
                 let iconurl = "http://openweathermap.org/img/wn/" + iconCode + ".png";
 
-                $('#currentIcon').attr('src', iconurl);
+                $("#currentIcon").attr("src", iconurl);
 
                 $("#currentCityHeader").text(cityName);
                 $("#currentCityTemp").text(cityTemp);
                 $("#currentCityHumidity").text(humidity);
                 $("#currentCityWind").text(windSpeed);
 
-                userTopCitiesArray.unshift(cityName)
-                localStorage.setItem("userCities", JSON.stringify(userTopCitiesArray))
+                userTopCitiesArray.unshift(cityName);
+                localStorage.setItem("userCities", JSON.stringify(userTopCitiesArray));
 
-                favriteCities()
-
-            })
+                favriteCities();
+                cityLat = response.coord.lat;
+                cityLon = response.coord.lon;
+                $.ajax({
+                    url: "http://api.openweathermap.org/data/2.5/onecall?lat=" +
+                        cityLat +
+                        "&lon=" +
+                        cityLon +
+                        "&units=imperial&APPID=e6f4fe18625540e700ef27c1af0d7303",
+                    method: "GET",
+                }).then(function(response) {
+                    console.log(response);
+                });
+            });
         }
     }
 
     function favriteCities() {
         for (let i = 0; i < 5; i++) {
             var cityNumber = "#city" + i;
-            $(cityNumber).text(userTopCitiesArray[i])
+            $(cityNumber).text(userTopCitiesArray[i]);
         }
-
     }
 
-
-    $(".cityName").on("click", cityHistoryButton)
+    $(".cityName").on("click", cityHistoryButton);
 
     function cityHistoryButton() {
-        $("#cityInput").val($(this).text())
-        callCityData()
+        $("#cityInput").val($(this).text());
+        callCityData();
     }
-
-
 });
