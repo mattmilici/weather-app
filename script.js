64f0d26844292976c3f27c1f560c21e2e177e4f7
Matthew Milici <milici.m915@gmail.com>
2020-08-07 20:23:39 -0700

    updated timer

--- script.js
+++ script.js
@@ -32,7 +32,9 @@ $(document).ready(function() {
             $.ajax({
                 url: "http://api.openweathermap.org/data/2.5/weather?q=" +
                     userCity +
-                    "&units=imperial&APPID=e6f4fe18625540e700ef27c1af0d7303",
+                    "," +
+                    userState +
+                    "us&units=imperial&APPID=e6f4fe18625540e700ef27c1af0d7303",
                 method: "GET",
             }).then(function(response) {
                 let cityName = response.name;
@@ -50,12 +52,6 @@ $(document).ready(function() {
                 $("#currentCityTemp").text(cityTemp);
                 $("#currentCityHumidity").text(humidity);
                 $("#currentCityWind").text(windSpeed);
-
-                let currentDateNumber = response.dt;
-                var CurrentTimestap = moment
-                    .unix(currentDateNumber)
-                    .format("MM/DD/YYYY");
-                $(".currentCityDate").text(CurrentTimestap);
 
                 userTopCitiesArray.unshift(cityName);
                 localStorage.setItem("userCities", JSON.stringify(userTopCitiesArray));
@@ -75,6 +71,12 @@ $(document).ready(function() {
                     for (let f = 1; f < 6; f++) {
                         let todayUVI = response.daily[0].uvi;
                         $("#currentCityUV").text(todayUVI);
+
+                        let currentDateNumber = response.daily[0].dt;
+                        var CurrentTimestap = moment
+                            .unix(currentDateNumber)
+                            .format("MM/DD/YYYY");
+                        $(".currentCityDate").text(CurrentTimestap);
 
                         let forecastDateId = ".forecastDate" + f;
                         let forecastIconId = ".forecastIcon" + f;
